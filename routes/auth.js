364600738f6fa88ba50ae48dfbb15d073c065027
Dell <abdoo.chbrik@gmail.com>
2021-05-14 13:43:22 +0200

    add auth/register

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req,res) => {
-    res.send("auth rooot")
+router.get("/register", async (req,res) => {
+
+    //res.send("auth rooot");
+    const user = await new User({
+        username: "chabrik",
+        email : "abdoo.chbrik@gmail.com",
+        password :"chabrik00"
+    })
+
+    await user.save();
+    res.send(ok);
+
+
 })
 
 module.exports = router
