fa3458ee7ba48f26ef7dc1456e80207b7618cfbf
Dell <abdoo.chbrik@gmail.com>
2021-05-19 01:22:58 +0200

    implement this fonctionnalties : create,delete,update,like,get a post

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 const bodyParser = require("body-parser")
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(morgan("common"))
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
 
 const db = require('./config/keys').mongoURI;
 
---
+++ routes/Posts.js
@@ -0,0 +1,102 @@
+const router = require("express").Router()
+const Post = require("../models/Post");
+const User = require("../models/User");
+const { get } = require("./users");
+
+//**************create a post *****////
+router.post("/", async (req,res) => {
+    const newPost = await new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//**************delete post ******//
+router.delete("/:id", async (req,res) => {
+    try {
+
+    //const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id);
+    if(req.body.userId === post.userId){
+        await post.deleteOne();
+       res.status(200).json("post has been deleted")
+    }
+    else{
+       res.status(403).json("you can delete only your posts")
+    }
+
+    } catch (error) {
+        res.status(500).json(error)   
+    }
+    
+})
+
+//**************update post ******//
+router.put("/:id", async (req,res) => {
+    try {
+
+    //const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id);
+    if(req.body.userId === post.userId){
+        await post.updateOne({$set : req.body});
+       res.status(200).json("post has been updated")
+    }
+    else{
+       res.status(403).json("you can update only your posts")
+    }
+
+    } catch (error) {
+        res.status(500).json(error)   
+    }
+    
+})
+
+//****************like and dislike a post  ******//
+router.put("/:id/like", async (req,res) => {
+
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+           await post.updateOne({ $push : { likes : req.body.userId } })
+           res.status(200).json("psot liked")
+        }
+        else {
+           await post.updateOne({ $pull : { likes : req.body.userId } })
+           res.status(200).json("post disliked")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+//***************** get a post ************//
+router.get("/:id", async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+//*************get posts of users followed ******//
+router.get("/friendPosts/all", async (req,res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId : currentUser._id});
+        const  friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId : friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+module.exports = router
